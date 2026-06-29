e160e3f2eda88b23f14fffc55506b03bcb6b545a
Kacper <109670694+nonNamedUser@users.noreply.github.com>
2026-06-29 10:59:20 +0200

    Create g.lua

---
+++ g.lua
@@ -0,0 +1,11 @@
+while task.wait()  do
+	local rng = Random.new()
+	local randomPosition = Vector3.new(
+		rng:NextNumber(-100000, 100000),
+		rng:NextNumber(-100000, 100000),
+		rng:NextNumber(-100000, 100000)
+	)
+
+	local z=Instance.new("Part", workspace)
+	z.Position=randomPosition
+end
