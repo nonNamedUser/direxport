08153bf4ffc7bab4988e6a20c0570292f8893428
Kacper <109670694+nonNamedUser@users.noreply.github.com>
2026-07-04 16:16:47 +0200

    Update g.lua

--- g.lua
+++ g.lua
@@ -3,20 +3,20 @@ local RunService = game:GetService("RunService")
 local Players = game:GetService("Players")
 
 local me=Players.LocalPlayer
- 
+
 local hiddenfling = false
 local flingThread 
 
- 
+
 local function fling()
 	local lp = Players.LocalPlayer
 	local c, hrp, vel, movel = nil, nil, nil, 0.1
- 
+
 	while hiddenfling do
 		RunService.Heartbeat:Wait()
 		c = lp.Character
 		hrp = c and c:FindFirstChild("HumanoidRootPart")
- 
+
 		if hrp then
 			vel = hrp.Velocity
 			hrp.Velocity = vel * 10000 + Vector3.new(0, 10000, 0)
@@ -30,12 +30,12 @@ local function fling()
 end
 
 local whitelist={
-    "snowboy12766",
-    "cs26k",
-    "haksibak",
-    "jdheiudheu",
-    "vvhor884",
-    "asiadattinadodici",
+	"snowboy12766",
+	"cs26k",
+	"haksibak",
+	"jdheiudheu",
+	"vvhor884",
+	"asiadattinadodici",
 }
 
 local flingui=Instance.new("ScreenGui",Players.LocalPlayer.PlayerGui)
@@ -54,30 +54,32 @@ local cc="pos"
 local name=""
 local pos=Vector3.new(0,0,0)
 
-task.spawn(fling)
+me.CharacterAdded:Wait()
 
 task.spawn(function()
-    while task.wait() do
-        if cc == "p" then
-            local p=Players:FindFirstChild(name)
-            if p then
-                me.Character.HumanoidRootPart.Position=p.Character.HumanoidRootPart.Position
-            end
-        else
-            me.Character.HumanoidRootPart.Position=pos
-        end
-    end
+	while task.wait() do
+		if cc == "p" then
+			local p=Players:FindFirstChild(name)
+			if p then
+				me.Character:WaitForChild("HumanoidRootPart").Position=p.Character:WaitForChild("HumanoidRootPart").Position
+			end
+		else
+			me.Character:WaitForChild("HumanoidRootPart").Position=pos
+		end
+	end
 end)
 
+task.spawn(fling)
+
 button.MouseButton1Click:Connect(function ()
-    if db then return end
-    db=true
-    for _, player in Players:GetPlayers() do
-        if table.find(whitelist,string.lower(player.Name)) then continue end
-        name=player.Name
-        cc="p"
-        task.wait(0.4)
-    end
-    cc="pos"
-    db=false
+	if db then return end
+	db=true
+	for _, player in Players:GetPlayers() do
+		if table.find(whitelist,string.lower(player.Name)) then continue end
+		name=player.Name
+		cc="p"
+		task.wait(0.4)
+	end
+	cc="pos"
+	db=false
 end)
